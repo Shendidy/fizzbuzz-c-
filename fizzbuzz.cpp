ffef251ee9c5c646253b4d02aa3ef3cea06fc26a
Shendidy <sherif@shendidy.com>
2019-05-28 00:52:05 +0100

    initial code to try running the file in terminal

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main()
 {
-  
+  int number;
+
+  cout << "Enter your number to use for FizzBuzz: " << endl;
+  cin >> number >> endl;
+
+  cout << number * 2 << endl;
 
   return 0
 }
